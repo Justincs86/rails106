b1a82d65c382f180eb522d12ef69576d1eb53a08
justin <davegle86@gmail.com>
2017-03-31 15:02:41 +0800

    use simple_format helper to decorate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
 
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :edit, :create, :update]
 
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
+    @post.group = @group
+    @post.user = current_user
   end
 
   def create
@@ -18,7 +20,36 @@ class PostsController < ApplicationController
     else
       render :new
     end
-    
+  end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+    @post.user = current_user
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Successful"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+    @post.user = current_user
+
+    @post.destroy
+    redirect_to account_posts_path, alert: "Post Deleted"
   end
 
 
